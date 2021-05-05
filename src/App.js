d888928f0908b36cbf27c6d5b10fba013cd83c41
sanket <sanketgargate06@gmail.com>
2021-05-05 15:17:03 +0530

    changed Router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import Maps from './components/Maps';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar } from './components/Navbar';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import { Collection } from './components/Collection';
 
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.BASEURL}>
         <Navbar />
         <Route path="/" exact component={Maps} />
         <Route path="/saved" component={Collection} />
